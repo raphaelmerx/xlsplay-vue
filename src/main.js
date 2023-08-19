5f9860e7a3a3e17156cfbb58c70dbe0e9e7d391c
Raphael Merx <raphael.merx@gmail.com>
2023-08-19 18:56:26 +0700

    Add logrocket

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import LogRocket from 'logrocket';
+
+LogRocket.init('jymlud/xlsplayraphadev');
 
 const vuetify = createVuetify({
   components,
